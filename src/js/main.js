b850328ca0e7d11d03428df2a69b07b6dc3520f0
Maribel López <maribel@maribel-msi-Thin>
2021-04-22 17:40:55 +0200

    Add projects

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const container = document.querySelectorAll(".js-image");
+const description = document.querySelectorAll(".js-text");
+
+function showText(event) {
+  const id = parseInt(event.currentTarget.id);
+  description[id].classList.toggle("js-projects");
+  description[id].classList.toggle("js-text");
+}
+function hideText(event) {
+  const id = parseInt(event.currentTarget.id);
+  description[id].classList.toggle("js-text");
+  description[id].classList.toggle("js-projects");
+}
+
+for (let i = 0; i < container.length; i++) {
+  container[i].addEventListener("mouseover", showText);
+  container[i].addEventListener("mouseout", hideText);
+}
